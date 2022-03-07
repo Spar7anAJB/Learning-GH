4e357c775921ac7ba4612aff63adf25a2cc05b25
Aidyn <aidyn.barley@gmail.com>
2022-03-07 12:49:05 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Aidyn Barley on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
